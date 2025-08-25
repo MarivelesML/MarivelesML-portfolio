6f86168204c3f774ba195ffd320ec0ea204353d6
mltmariveles <mariveles.marklester@ue.edu.ph>
2025-08-25 21:02:02 +0800

    Improve nav bar

--- src/js/main.js
+++ src/js/main.js
@@ -5,26 +5,6 @@
 //   ease: "power2.out",
 // });
 
-//The from() method is used to animate a single element from an ending state to a starting state.
-
-gsap.from(".nav-left", {
-  opacity: 0,
-  x: -500,
-  duration: 3,
-});
-
-gsap.from(".nav-center", {
-  opacity: 0,
-  y: 500,
-  duration: 3,
-});
-
-gsap.from(".nav-right", {
-  opacity: 0,
-  x: 500,
-  duration: 3,
-});
-
 // gsap.fromTo(
 //   ".nav-left",
 //   { opacity: 0, x: -500 },
@@ -55,3 +35,51 @@ gsap.from(".nav-right", {
 //     duration: 3,
 //   }
 // );
+
+//The from() method is used to animate a single element from an ending state to a starting state.
+
+// gsap.from(".nav-left", {
+//   opacity: 0,
+//   y: 70,
+//   duration: 1.5,
+//   ease: "power2.out",
+// });
+
+// gsap.from(".nav-center", {
+//   opacity: 0,
+//   y: 80,
+//   duration: 2.0,
+//   ease: "power2.out",
+// });
+
+// gsap.from(".nav-right", {
+//   opacity: 0,
+//   y: 90,
+//   duration: 2.5,
+//   ease: "power2.out",
+// });
+
+gsap.set(".cus-btn", {
+  opacity: 0,
+  y: 100,
+});
+
+gsap.to(".cus-btn", {
+  opacity: 1,
+  y: 0,
+  duration: 1.5,
+  ease: "power2.out",
+  stagger: {
+    amount: 3.5,
+    each: 0.5,
+    from: 0,
+  },
+});
+
+document.getElementById("emblem").addEventListener("click", function () {
+  gsap.to(".cus-emblem", {
+    rotation: "+=180",
+    duration: 1,
+    ease: "power2.inOut",
+  });
+});
